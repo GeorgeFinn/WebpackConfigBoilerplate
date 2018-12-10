a3897fa703d24e106df9b20489b58b89fde3fad6
GeorgeFinn <george.finn@me.com>
2018-12-09 23:39:15 -0500

    Added React, Babel for react

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 require("webpack-hot-middleware/client?reload=true")
 require('./main.css')
 require('./index.html')
+require('./app.js')
 
 var a = async () => {
   await console.log('hello from the future')
