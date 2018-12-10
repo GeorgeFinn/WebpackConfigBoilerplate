f3df5e32f2816cad87e28a9c57d526cdcc94feac
GeorgeFinn <george.finn@me.com>
2018-12-09 23:02:28 -0500

    finished babel

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,16 +1,57 @@
-const path = require('path')
+const path = require("path")
 
 module.exports = {
   entry: {
-    main: "./src/main.js"
+    main: ["@babel/polyfill", "./src/main.js"]
   },
   mode: "development",
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name]-bundle.js",
     path: path.resolve(__dirname, "../dist"),
     publicPath: "/"
   },
   devServer: {
-    contentBase: "dist"
+    contentBase: "dist",
+    overlay: true
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]"
+            }
+          },
+          {
+            loader: "extract-loader",
+            options: {
+              publicPath: "../"
+            }
+          },
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
   }
 }
---
+++ src/main.js
@@ -0,0 +1,8 @@
+require('./main.css')
+require('./index.html')
+
+
+var a = async () => {
+  await console.log('hello from the future')
+  console.log('done')
+}
